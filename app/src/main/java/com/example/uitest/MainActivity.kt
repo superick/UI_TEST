f9ba8fa8714f1b400f3110618da9c79d749b4890
superick <superick@naver.com>
2022-02-07 20:17:03 +0900

    코틀린 동작 버튼 누르기

--- app/src/main/java/com/example/uitest/MainActivity.kt
+++ app/src/main/java/com/example/uitest/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.uitest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//   확인 버튼이 누르면 이벤트 달아주자.
+
+        okBtn.setOnClickListener {
+
+            val inputContent = contentEdt.text.toString()
+
+            resultTxt.text = inputContent
+        }
+
+
+        }
 
 
     }
-}
